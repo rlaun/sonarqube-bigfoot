d86b7d509759541995e4d7b0e0ff8c364be5e36f
rlaun <roland.laun@thalesgroup>
2017-12-09 13:25:49 +0100

    issue lifecycle test - step 1

---
+++ src/main/java/sq/bigfoot/App.java
@@ -0,0 +1,15 @@
+package aa;
+
+/**
+ * Hello world
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        int magicNumber = 42;
+
+        String s = new String("Very long line that does not meet our maximum 120 character line length criteria and should be wrapped to avoid SonarQube issues.");
+    }
+}
